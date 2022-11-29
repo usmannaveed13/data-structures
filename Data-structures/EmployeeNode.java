42b671d86e0bea5469b8a01f7bc9e57c055f1246
usmannaveed13 <usmannaveed13@gmail.com>
2022-11-29 17:48:35 +0500

    data structure (Doubly LinkedList)

--- Data-structures/EmployeeNode.java
+++ Data-structures/EmployeeNode.java
@@ -1,6 +1,7 @@
 public class EmployeeNode {
     private Employee employee;
     private EmployeeNode next;
+    private EmployeeNode previous;
 
     public EmployeeNode(Employee employee) {
         this.employee = employee;
@@ -20,6 +21,14 @@ public class EmployeeNode {
 
     public void setNext(EmployeeNode next) {
         this.next = next;
+    }
+
+    public EmployeeNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(EmployeeNode previous) {
+        this.previous = previous;
     }
 
     public String toString() {
